535cf8770998615e288213a0e6d525492c355eae
Ajaynirm <ajaysam397@gmail.com>
2024-12-26 21:35:45 +0530

    files structured for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: import.meta.env.MODE === "development"? "http://localhost:5001/api":"/api",
     //to add cookoies for every request ,so i use , essential for private api access
     withCredentials: true
 });
