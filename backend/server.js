352b8de2440eb0e0adc1629a40fa63794ff162a1
Ajaynirm <ajaysam397@gmail.com>
2025-02-07 09:45:01 +0530

    frontend and backend merged

--- backend/server.js
+++ backend/server.js
@@ -38,13 +38,6 @@ app.use("/api/laptop", laptopRoute);
 app.use("/api/maintain", maintainanceRoutes);
 app.use("/api/assignment", assignmentRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 app.listen(port, () => {
   console.log("server is running on PORT:" + port);
