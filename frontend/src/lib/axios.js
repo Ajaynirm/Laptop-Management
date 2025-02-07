dec523200d442db258898c737b514e62540da650
Ajaynirm <ajaysam397@gmail.com>
2025-02-07 09:39:58 +0530

    frontend and backend merged

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development"? "http://localhost:5001/api":"/api",
+    baseURL: import.meta.env.MODE === "development"? "http://localhost:5001/api":"https://laptop-management-3gx1.onrender.com/api",
     //to add cookoies for every request ,so i use , essential for private api access
     withCredentials: true
 });
+
+
